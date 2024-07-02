cee2260e5b61385a5a8cbb4da40cf887a9297733
Igor Koneshov <igorkoneshov@mail.ru>
2024-07-03 01:13:54 +0400

    fix: delay after open file/folder

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,23 +1,22 @@
-require("neo-tree").setup({
-
+require("neo-tree").setup {
     filesystem = {
-	source_selector = {
-			winbar = false,
-			statusline = false
-	},
-	window = {
-        mapping_options = {
-            noremap = true,
-            nowait = true,
-         },
-        mappings = {
-					  ["<F5>"] = "refresh",
-            ["p"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
-            ["o"] = { command = "open", nowait = true },
+	    source_selector = {
+		    	winbar = false,
+			    statusline = false
+	    },
+    	window = {
+            mapping_options = {
+                noremap = true,
+                nowait = true,
+             },
+            mappings = {
+			    ["<F5>"] = "refresh",
+                ["p"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+                ["o"] = { command = "open", nowait = true },
+            }
         }
-  }
-  }
-})
+    }
+}
 
 -- NeoTree
 vim.keymap.set('n', '<leader>e', ':Neotree float toggle<CR>')
