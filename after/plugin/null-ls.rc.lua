494d5bd03a20d80ca075970480a44b3a90dfb8f9
Igor Koneshov <igorkoneshov@mail.ru>
2024-08-10 15:32:30 +0400

    fix: disable auto format after auto save

--- after/plugin/null-ls.rc.lua
+++ after/plugin/null-ls.rc.lua
@@ -3,12 +3,13 @@ if (not status) then return end
 
 null_ls.setup {
   on_attach = function(client, bufnr)
-    if client.server_capabilities.documentFormattingProvider then
-      vim.api.nvim_command [[augroup Format]]
-      vim.api.nvim_command [[autocmd! * <buffer>]]
-      vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
-      vim.api.nvim_command [[augroup END]]
-    end
+    -- Auto format after save. ( Disabled because he broke auto-save )
+    -- if client.server_capabilities.documentFormattingProvider then
+    --   vim.api.nvim_command [[augroup Format]]
+    --   vim.api.nvim_command [[autocmd! * <buffer>]]
+    --   vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+    --   vim.api.nvim_command [[augroup END]]
+    -- end
   end,
   sources = {
     null_ls.builtins.diagnostics.eslint_d.with({
