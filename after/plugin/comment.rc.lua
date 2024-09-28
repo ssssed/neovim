b8110ce3f2db3e4f082ba2751e774ce474160638
Igor Koneshov <igorkoneshov@mail.ru>
2024-09-28 23:40:00 +0400

    feat: add storybook snip

--- after/plugin/comment.rc.lua
+++ after/plugin/comment.rc.lua
@@ -3,10 +3,10 @@ if (not status) then return end
 
 comment.setup {
   toggler = {
-    line = '<C-/>',
-    block = '<C-b>/'
+    line = 'cl',
+    block = 'cb'
   },
-  opleader = { line = '<C-/>', block = '<C-b>/' },
+  opleader = { line = 'cl', block = 'cb' },
   mappimgs = {
     basic = true,
     extra = false
--- lua/snippets.lua
+++ lua/snippets.lua
@@ -70,7 +70,26 @@ ls.add_snippets("typescript", {
     t({ "", "    <div></div>" }),
     t({ "", "  )" }),
     t({ "", "}" }),
-  })
+  }),
+  s("stories", {
+    t("import type { Meta, StoryObj } from '@storybook/react';"),
+    t({ "", "import { " }), i(1, "ComponentName"), t(" } from './"), i(1), t("';"),
+    t({ "", "" }),
+    t({ "", "" }),
+    t("const meta: Meta<typeof "), i(1), t("> = {"),
+    t({ "", "  component: " }), i(1), t(","),
+    t({ "", "  tags: ['autodocs']" }),
+    t({ "", "};" }),
+    t({ "", "" }),
+    t({ "", "" }),
+    t("export default meta;"),
+    t({ "", "" }),
+    t({ "", "" }),
+    t("type Story = StoryObj<typeof "), i(1), t(">;"),
+    t({ "", "" }),
+    t({ "", "" }),
+    t("export const Default: Story = {};"),
+  }),
 })
 
 
@@ -105,5 +124,20 @@ ls.add_snippets("typescriptreact", {
     t({ "", "    <div></div>" }),
     t({ "", "  )" }),
     t({ "", "}" }),
-  })
+  }),
+  s("stories", {
+    t("import type { Meta, StoryObj } from '@storybook/react';"),
+    t({ "", "import { " }), i(1, "ComponentName"), t(" } from './"), i(1), t("';"),
+    t({ "", "" }),
+    t("const meta: Meta<typeof "), i(1), t("> = {"),
+    t({ "", "  component: " }), i(1), t(","),
+    t({ "", "  tags: ['autodocs']" }),
+    t({ "", "};" }),
+    t({ "", "" }),
+    t("export default meta;"),
+    t({ "", "" }),
+    t("type Story = StoryObj<typeof "), i(1), t(">;"),
+    t({ "", "" }),
+    t("export const Default: Story = {};"),
+  }),
 })
